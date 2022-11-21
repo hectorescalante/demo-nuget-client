417efc859b33cd4fa1018dbdfe848978902a83eb
Hector Escalante <hector.samuel.escalante@outlook.com>
2022-11-21 20:01:49 +0100

    add demo library

---
+++ src/Demo.NuGet.Client/ApiClient.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo.NuGet.Client
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task HealthCheck()
+        {
+            await _httpClient.GetAsync("http://localhost/healthz");
+        }
+    }
+}
